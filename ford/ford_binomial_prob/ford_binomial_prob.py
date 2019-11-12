b817aeb14f704fb732a71ac98b1db66e82d025b9
davisschenk <35881002+davisschenk@users.noreply.github.com>
2019-11-12 08:00:18 -0700

    Improved ford binomial probability

--- ford/ford_binomial_prob/ford_binomial_prob.py
+++ ford/ford_binomial_prob/ford_binomial_prob.py
@@ -2,17 +2,26 @@ from scipy.stats import binom
 import plotly.graph_objects as go
 
 more = [binom.pmf(x, 100, 0.7186) for x in range(100)]
-print(more)
+less = [binom.pmf(x, 100, 0.2814) for x in range(100)]
+
 fig = go.Figure()
 fig.add_trace(
     go.Bar(
+        name=">35k",
         x=list(range(100)),
         y=[x*100 for x in more]
     )
 )
+fig.add_trace(
+    go.Bar(
+        name="<35k",
+        x=list(range(100)),
+        y=[x*100 for x in less]
+    )
+)
 
 fig.update_layout(
-    title="Probability of exactly N F150s being selected out of 100",
+    title="Ford F150 Binomial PDF out of 100",
     xaxis_title="N (Number of F150s)",
     yaxis_title="Probability",
     yaxis=dict(
